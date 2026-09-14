f54b071e223809a4f2b68dfba1ec623c97788a2d
ZAKARY BRETON <zbreton@etu.uqac.ca>
2026-09-14 18:42:28 -0400

    Adding hello world

---
+++ hello_world.py
@@ -0,0 +1,11 @@
+this_is_a_variable = 1 #ok
+
+thisIsAVariable = 1 #not ok
+
+def my_good_function(): #ok
+    print("wright")
+
+def myFunction(): #not ok
+    print("wrong")
+
+my_good_variable = "good" #ok
