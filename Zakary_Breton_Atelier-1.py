ca1d5329d58993a959d47e30a2a0b296e3035ae5
ZAKARY BRETON <zbreton@etu.uqac.ca>
2026-09-21 16:49:37 -0400

    add atelier 1

---
+++ Zakary_Breton_Atelier-1.py
@@ -0,0 +1,10 @@
+
+# lire l'input
+name = input("Qui etes-vous?! ")
+
+# greetings
+print("Bienvenue a cactusity "  + name + "!")
+
+origine = input("Et d'où viens tu " + name + " ? ")
+
+print(origine + " C'est loins en chien!")
