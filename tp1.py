42b544bfa2104db4b3da2a374da574c3b42e91a0
plutogamedev <zak_breton24@hotmail.com>
2026-09-21 13:24:23 -0400

    add some comments

--- tp1.py
+++ tp1.py
@@ -2,7 +2,6 @@ import sys
 import json
 import os
 
-from PySide6 import QtWidgets
 from PySide6.QtWidgets import QApplication, QTableWidget, QTableWidgetItem, QLineEdit, QVBoxLayout, QWidget, QLabel
 
 
@@ -41,7 +40,7 @@ tableau.setRowCount(len(data))
 tableau.setColumnCount(len(data[0]))
 tableau.setHorizontalHeaderLabels(data[0].keys())
 
-#affice le nom du fichier
+#affihe le nom du fichier
 nom_fichier.setText("Nom du fichier : " + os.path.basename(sys.argv[1]))
 
 #affiche le poid du fichier
@@ -50,8 +49,10 @@ poid_du_fichier.setText("taille : " + str(information) + " octets")
 #affiche le nombre d'éléments
 nombre_elements.setText("nombre d'éléments : " + str(total))
 
-#place les valeurs dans les bonnes cases
+#passe chaque ligne de data et me donne le numéro de ligne et le contenu de la ligne elle même
 for numero_ligne, ligne in enumerate(data):
+    #passe chaque colone et donne l'ensemble de nom de champ et de valeur et ensuite lui associe un numéro avec enumerate
+    #numero_colone = la position de la colonne, nom_colone = le titre du champ, valeur = le contenu
     for numero_colone, (nom_colone, valeur) in enumerate(ligne.items()):
 
         item_valeur = QTableWidgetItem(str(valeur))
@@ -60,22 +61,28 @@ for numero_ligne, ligne in enumerate(data):
         
 
 
-
+#fonction qui vérifie chaque case et me dit si l'une des cases correspond au texte que je recherche
 def rechercher_dans_tableau(texte):
 
+    #passe chaque ligne selon le nombre de ligne dans le tableau
     for ligne in range (tableau.rowCount()):
         correspond = False
 
+        #passe chaque colone selon le nombre de colone dans le tableau
         for colone in range (tableau.columnCount()):
 
+            #stock l'item dans la variable case pour chaque croisement ligne colone
             case = tableau.item(ligne, colone)
+
+            #mets tout en minuscule et verifie si le texte rechercher est contenue dans la case
             if  case and texte.lower() in case.text().lower():
                 correspond = True
                 break
 
+        #cache les lignes qui ne correspondent pas
         tableau.setRowHidden(ligne, not correspond)
 
-
+#a chaque fois que je tappe quelque chose dans la barre de recherche ça lance la fonction rechercher_dans_tableau
 barre_de_recherche.textChanged.connect(rechercher_dans_tableau)
 
 #active le trie du tableau
